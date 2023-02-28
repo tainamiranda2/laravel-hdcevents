7f197acf7edb0f04dc6e466ebd833bdab02c343d
Tainá Miranda <tainaferreira0987@gmail.com>
2023-02-28 15:21:06 -0300

    assunto blades

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //blade 
+    $nome = "matheus";
+    $idade=29;
+    $arr=[1,2,3,4,5];
+
+    return view('welcome',
+[
+    'nome'=>$nome,
+    'idade2'=>$idade,
+    'profissao'=>"PRogramação",
+    'arr'=>$arr
+]
+);
+
 });
 
 Route::get('/contact', function () {
